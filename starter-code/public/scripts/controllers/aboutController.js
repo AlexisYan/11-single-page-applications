36681a15e07f5b2ad4a83890457dd9a30ceafd44
Alexis Yan <yanranalexis@gmail.com>
2017-05-22 15:34:14 -0700

    last commit

--- starter-code/public/scripts/controllers/aboutController.js
+++ starter-code/public/scripts/controllers/aboutController.js
@@ -4,8 +4,11 @@ var app = app || {};
 (function(module) {
   const aboutController = {};
 
-  // TODO: Define a function that hides all main section elements, and then reveals just the #about section:
-function()
-
+  // Done: Define a function that hides all main section elements, and then reveals just the #about section:
+  aboutController.init = function() {
+    app.Article.fetchAll(app.articleView.initArticleViewPage);
+    $('.tab-content').hide();
+    $('#about').fadeIn();
+  }
   module.aboutController = aboutController;
 })(app);
